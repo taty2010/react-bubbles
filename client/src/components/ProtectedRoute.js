9614608848951c256729ae25534c07dc5de58f28
taty2010 <shydalys123@gmail.com>
2020-02-22 11:34:39 -0600

    added protected route and axiosWithAuth

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import BubblePage from './BubblePage';
+
+const ProtectedRoute = ({compoenent: Component, ...props}) => {
+    return(
+        <Route
+            {...props}
+            render={()=>{
+                if (localStorage.getItem('token')){
+                    return <Component/>
+                }else{
+                    return <Redirect to='/login' />
+                }
+            }}
+
+            />
+    )
+}
